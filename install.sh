a3eff3b0a6f0f372e526e1e51b712872d8bd1443
Alex Pliutau <a.pliutau@gmail.com>
2017-08-15 23:05:50 +0700

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Author: Alex Pliutau http://pliutau.com
+
+set -e
+
+downloadFile() {
+	curl -L "https://raw.githubusercontent.com/plutov/o/master/o.sh" -o /tmp/o.sh
+}
+
+installFile() {
+	chmod +x /tmp/o.sh
+	sudo mv /tmp/o.sh /usr/local/bin/o
+}
+
+downloadFile
+installFile
