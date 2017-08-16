1f451d1cba7a436bc37a34f7a240da1caa93c911
Alex Pliutau <a.pliutau@gmail.com>
2017-08-16 10:02:29 +0700

    Add support of ssh:// and Stash

--- o.sh
+++ o.sh
@@ -10,12 +10,20 @@ get_url() {
 		remote=${remote/git\@github\.com\:/https://github.com/}
 		remote=${remote/git\@bitbucket\.org\:/https://bitbucket.org/}
 		remote=${remote/git\@gitlab\.com\:/https://gitlab.com/}
-		remote=${remote/\.git//}
+		remote=${remote/\.git/}
+		remote=${remote/\.git/}
+		remote=${remote/git\@/}
+		remote=${remote/ssh\:\/\//http:\/\/}
+		domain=$(echo "$remote" | awk -F/ '{print $3}')
+		if [[ $domain == *"stash"* ]]; then
+			remote=${remote/$domain/$domain\/scm}
+		fi
 		echo $remote
 	fi
 }
 
 url=$(get_url)
+echo $url
 if [ "" != "$url" ]; then
 	open $url
 fi
