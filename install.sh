ee40cfb6411c73790938ab4300329e39f854dfde
Alex Pliutau <a.pliutau@gmail.com>
2017-08-17 12:04:52 +0700

    Add wget support to install.sh

--- install.sh
+++ install.sh
@@ -4,19 +4,27 @@
 
 set -e
 
+DOWNLOAD_URL="https://raw.githubusercontent.com/plutov/o/master/o.sh"
+TMP_FILE="/tmp/o.sh"
+BIN_FILE="/usr/local/bin/o"
+
 downloadFile() {
-	curl -L "https://raw.githubusercontent.com/plutov/o/master/o.sh" -o /tmp/o.sh
+	if type "curl" > /dev/null; then
+		curl -L "$DOWNLOAD_URL" -o "$TMP_FILE"
+	elif type "wget" > /dev/null; then
+		wget -q -O "$TMP_FILE" "$DOWNLOAD_URL"
+	fi
 }
 
 installFile() {
-	chmod +x /tmp/o.sh
+	chmod +x "$TMP_FILE"
 	SUDO=''
 	if [ $EUID != 0 ]; then
 		SUDO='sudo'
 	fi
-	$SUDO mv /tmp/o.sh /usr/local/bin/o
+	$SUDO mv "$TMP_FILE" /usr/local/bin/o
 }
 
 downloadFile
 installFile
-echo "Installed in /usr/local/bin/o"
+echo "Installed in $BIN_FILE"
